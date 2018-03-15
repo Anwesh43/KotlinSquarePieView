cda695e8224f978f5a29d392bcce65c7b315eef8
Anwesh43 <anweshthecool0@gmail.com>
2018-03-15 09:10:37 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlinsquarepieview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsquarepieview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinsquarepieview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.squarepieview.SquarePieView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquarePieView.create(this)
     }
 }
