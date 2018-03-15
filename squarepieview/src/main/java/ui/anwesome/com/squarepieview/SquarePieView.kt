b0ad9f1f388a941609433416ee60dc3cce250a0e
Anwesh43 <anweshthecool0@gmail.com>
2018-03-15 08:28:19 +0530

    adding SquarePie class

--- squarepieview/src/main/java/ui/anwesome/com/squarepieview/SquarePieView.kt
+++ squarepieview/src/main/java/ui/anwesome/com/squarepieview/SquarePieView.kt
@@ -20,7 +20,7 @@ class SquarePieView(ctx : Context) : View(ctx) {
         return true
     }
     data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0, var jDir : Int = 1) {
-        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f)
         fun update(stopcb : (Float) -> Unit) {
             scales[j] += 0.1f * dir
             if (Math.abs(scales[j] - prevScale) > 1) {
@@ -66,6 +66,39 @@ class SquarePieView(ctx : Context) : View(ctx) {
                 animated = true
                 view.postInvalidate()
             }
+        }
+    }
+    data class SquarePie(var i : Int, var state : State = State()) {
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w = canvas.width.toFloat()
+            val h = canvas.height.toFloat()
+            val size = Math.min(w,h)/ 5
+            paint.color = Color.parseColor("#ef5350")
+            paint.style = Paint.Style.STROKE
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeWidth = Math.min(w, h) / 50
+            val sx = -(size/2) * state.scales[1]
+            canvas.save()
+            canvas.translate(w/2 , h/2)
+            for(i in 0..1) {
+                val sf = 1f - 2 * i
+                for(i in 0..1) {
+                    canvas.save()
+                    canvas.translate(sx * sf, size/2 * sf)
+                    canvas.rotate(90f * i * state.scales[2])
+                    canvas.drawLine(0f, 0f, 0f, -size * sf * state.scales[0], paint)
+                    canvas.restore()
+                }
+            }
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-size/3, -size/3, size/3, size/3), 0f, 360f * state.scales[3], true, paint)
+            canvas.restore()
+        }
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
         }
     }
 }
