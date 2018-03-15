1ea83dfef58f32a8754c28a48ce482b2d9001a46
Anwesh43 <anweshthecool0@gmail.com>
2018-03-15 08:03:27 +0530

    adding State class

--- squarepieview/src/main/java/ui/anwesome/com/squarepieview/SquarePieView.kt
+++ squarepieview/src/main/java/ui/anwesome/com/squarepieview/SquarePieView.kt
@@ -19,4 +19,28 @@ class SquarePieView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0, var jDir : Int = 1) {
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += jDir
+                if (dir == 0f) {
+                    jDir *= -1
+                    dir = 0f
+                    j += jDir
+                    prevScale = scales[j]
+                    stopcb(scales[j])
+
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f
+                startcb()
+            }
+        }
+    }
 }
