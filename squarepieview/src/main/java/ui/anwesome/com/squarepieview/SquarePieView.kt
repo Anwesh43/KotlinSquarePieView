c2d8361d628af30ac9b588273b2cf558aa8486f1
Anwesh43 <anweshthecool0@gmail.com>
2018-03-15 07:56:14 +0530

    adding SquarePieView class

--- squarepieview/src/main/java/ui/anwesome/com/squarepieview/SquarePieView.kt
+++ squarepieview/src/main/java/ui/anwesome/com/squarepieview/SquarePieView.kt
@@ -6,3 +6,17 @@ package ui.anwesome.com.squarepieview
 import android.content.*
 import android.view.*
 import android.graphics.*
+class SquarePieView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
